537ec9a68046b21ff08371d9467a3723c6e63a9a
Imran Hossen <freelancerimran81@gmail.com>
2022-07-13 02:33:56 +0600

    first commit

---
+++ client-side/src/Components/Home/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+
+            <button class="btn btn-primary mt-10">Button</button>
+            <button class="btn btn-secondary">Button</button>
+            <button class="btn btn-accent">Button</button>
+            <button class="btn btn-neutral">Button</button>
+            <button class="btn btn-info">Button</button>
+            <button class="btn btn-success">Button</button>
+            <button class="btn btn-warning">Button</button>
+            <button class="btn btn-error">Button</button>
+        </div>
+    );
+};
+
+export default Home;
---
+++ client-side/tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#f79f1f",
+          secondary: "#130f40",
+          accent: "#666666",
+          neutral: "#F3F4F6",
+          "base-100": "#f7f7f7",
+          info: "#98A8DD",
+          success: "#27ae60",
+          warning: "#DF7E07",
+          error: "#FA5C5C",
+        },
+      },
+      {
+        dark: {
+          primary: "#F59E0B",
+          secondary:"#ffff",
+          accent: "#666666",
+          neutral: "#dfdfef",
+          "base-100": "#090A27",
+          info: "#98A8DD",
+          success: "#1BBB70",
+          warning: "#F59E0B",
+          error: "#FB7185",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
